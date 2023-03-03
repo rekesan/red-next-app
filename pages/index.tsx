14ba92c50173e33d1cddbaf7e229feef3f5522c7
rekesan <redkeans@gmail.com>
2023-03-03 09:46:25 +0800

    removed onclick effect, added hover aniamtion

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,18 +3,16 @@ import { colorList, ColorSet } from "./data";
 
 export default function Home() {
   const [backgroundColor, setBG] = useState(colorList[9].backgroundColor);
-  const [backgroundColorButton, setBGButton] = useState(colorList[9].buttonColor);
+  const [backgroundColorButton, setBGButton] = useState(
+    colorList[9].buttonColor
+  );
   const [textColor, setFontColor] = useState(colorList[9].textColor);
-
-  const [effect, setEffect] = useState(false);
 
   const changeColor = () => {
     const newColor = getNewColor();
     setBG(newColor.backgroundColor);
     setBGButton(newColor.buttonColor);
     setFontColor(newColor.textColor);
-
-    setEffect(false);
   };
 
   const getNewColor = () => {
@@ -26,16 +24,14 @@ export default function Home() {
   };
 
   return (
-    <div className={`${backgroundColor} h-screen flex justify-center items-center`}>
+    <div
+      className={`${backgroundColor} h-screen flex justify-center items-center`}
+    >
       <button
-        id="haha"
-        className={`${backgroundColorButton} ${textColor} border-4 p-2 h-16 w-64 tracking-widest rounded-full font-bold text-xl  ${
-          effect && "animate-side2side"
-        }`}
-        onClick={() => setEffect(true)}
-        onAnimationEnd={changeColor}
+        className={`${backgroundColorButton} ${textColor} hover:animate-pulse border-4 p-2 h-16 w-64 tracking-widest rounded-full font-bold text-xl`}
+        onClick={changeColor}
       >
-        PALIT KULAY
+        {backgroundColor}
       </button>
     </div>
   );
